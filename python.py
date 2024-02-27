b65e2214fe1b5cf98092592b4da35a61eac1f0e0
NOTYOUONI1 <sorry56363472@gmail.com>
2024-02-27 10:19:04 +0600

    pythone

---
+++ python.py
@@ -0,0 +1,18 @@
+print("name")
+print("Rakib")
+print(50)
+print(70+30)
+name = "rakib"
+age = 158
+price = 1000000000000000000000000000000000000000000.0
+print("my name is :", name)
+print("my age :", age)
+print("this boy price is :", price)
+age2 = age
+print(type(name))
+print(type(age))
+print(type(price))
+name1 = "rk"
+age1 = 15
+price3 = 12.2
+print(name1, age1, price3)
