5321715147b07764d79d416df5c4486c919644f5
NOTYOUONI1 <sorry56363472@gmail.com>
2024-02-29 17:57:00 +0600

    29

--- python.py
+++ python.py
@@ -34,7 +34,7 @@ elif(marks >= 80 ):
 
 elif(marks >= 70):
     print("B")
-    
+
     print("D")
     print("this ragal is", marks) # type: ignore'''
 '''age = int(input("age :"))
@@ -43,8 +43,72 @@ print(vote)'''
 '''b = 4
 h = 9
 print(b + h)'''
-a = 2000
+'''a = 2000
 b = 2000
 mun = 10
-mim = mun + 10
+mim = mun * 100 #1000
 print("mim :", mim)
+d = input("what you :")
+print("lolsa :", d)'''
+'''a = int(input("Enter your number :"))
+b = int(input("+ :"))
+
+print("low :", a + b)'''
+#a = int(input("Enter :"))
+#b = int(input("Enter :"))
+'''print("Hello\nWorld")
+print('rakib\nWijv')
+h = "lkjopfdglkjfglkmnfbglkjfbglkjfblkjfbgsffdsglj;mfbg\ngifigfjhfdjfvdkjnvfkjnvclknvclknxcvlkn"
+print(h)
+jj = "rakib"
+print(jj[2:])'''
+'''ri = "rakib"
+print(i.count("ONI"))'''
+'''p = "rakib ka sa ka sa ka sa"
+print(p.count("k"))'''
+#y = input("name :")
+#print("my name is :", y)
+"""i = input("rakib :")
+if(i == "r"):
+    print("s")
+if(i == "y"):
+    print("st")
+if(i == "u"):
+    print("sto")"""
+"""if(maks < 70):
+    print("c")
+elif(maks < 80):
+    print("b")
+elif(maks < 90):
+    print("A")
+elif(maks < 100):
+    print("A+")"""
+"""maks = int(input("enter maks :"))
+if(maks > 70):
+    print("u")"""""""""
+"""a = int(input("enter your number :"))#9
+b = int(input("enter your number :"))#5
+c = int(input("enter your number :"))#8
+d = int(input("enter your number :"))#8
+if(a > b and a < c):""""""
+    print("rezalt :",a)
+elif(b > c and b < d):
+    print("rezalt ",b)
+elif(c > d ):
+    print("rezalt ",c)
+else:
+    print("no",d)"""
+"""marks = [65,5454,4555636,346,56]
+print(marks)
+s = ["rakib",786,4325,"ONI"]
+s[3] = "ONI"
+print(s)
+i = "rakib", 22,
+print(i[1])"""
+"""i = (535,739485676,6426333356072356,87)
+print(type(i))
+print(i[0])"""
+a = input("cartoon name:")
+b = int(input("ep :"))
+c = input("DETALs :")
+print("cartoon", a, "|" "BEST ep", b, "|" "DETAL", c)
